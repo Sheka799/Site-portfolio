722396ec8ac6d6e07677e8f4acaac1cc6ef4c11e
unknown <sheka1991_91@mail.ru>
2020-06-23 16:49:16 +0300

    feature:link

--- js/style.js
+++ js/style.js
@@ -1,22 +1,16 @@
-// $(".open-menu").click(function() {
-// 	$(this).toggleClass("active");
-// 	$(".popup-overlay").toggleClass("open");
-// 	// this line ▼ prevents content scroll-behind
-// 	$("body").toggleClass("locked"); 
-// });
 
-const openMenu = document.querySelector('.open-menu'),
+  const openMenu = document.querySelector('.open-menu'),
   popupOverlay = document.querySelector('.popup-overlay'),
   body = document.body;
-
-openMenu.classList.add('open-popup-btn');
-
-const link = document.querySelectorAll('.popup-menu__link');
- for (let i=0; i < link.length; i++) {
+  
+  openMenu.classList.add('open-popup-btn');
+  
+  const link = document.querySelectorAll('.popup-menu__link');
+  for (let i=0; i < link.length; i++) {
   link[i].classList.add('link');
- }
-
-window.addEventListener('click', (event) => {
+  }
+  
+  window.addEventListener('click', (event) => {
   const target = event.target;
   if (target.classList.contains('open-popup-btn')) {
     openMenu.classList.add('active');
@@ -37,4 +31,5 @@ window.addEventListener('click', (event) => {
     openMenu.classList.add('open-popup-btn')
     openMenu.classList.remove('close-popup-btn')
   }
-})
+  });
+  
